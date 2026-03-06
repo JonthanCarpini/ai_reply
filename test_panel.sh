b6457061e448bd2162cee08bb4c60258c54df337
Carpini01 <113296584+Carpini01@users.noreply.github.com>
2026-03-06 18:32:08 -0300

    chore: Remover script de teste temporário

--- test_panel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-API_KEY='6036b03e809102e1c844ca0d991ff49ba3ea472f398fefb02c1ebb7d12d0cb7d'
-B64=$(php -r "echo base64_encode(json_encode(['api_key'=>'$API_KEY']));")
-echo "=== Teste 1: JSON direto ==="
-curl -s -X POST https://xpainel.online/api/reseller/credits -H 'Content-Type: application/json' -d "{\"api_key\":\"$API_KEY\"}"
-echo ""
-echo "=== Teste 2: Base64 IBO ==="
-curl -s -X POST https://xpainel.online/api/reseller/credits -H 'Content-Type: application/json' -d "{\"data\":\"$B64\"}"
-echo ""
