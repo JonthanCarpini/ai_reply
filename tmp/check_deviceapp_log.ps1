ee46f3d7be305224184e75a4bc75dab3b1a1b805
Carpini01 <113296584+Carpini01@users.noreply.github.com>
2026-03-11 12:11:32 -0300

    chore: remove temporary debug files

--- tmp/check_deviceapp_log.ps1
+++
@@ -1,2 +0,0 @@
-$remoteCommand = "docker compose -f /opt/ai_reply/docker-compose.yml exec -T ai_reply_api sh -lc 'grep -n \"\\[DeviceApp\\]\" storage/logs/laravel.log | tail -30'"
-ssh root@207.180.228.84 $remoteCommand
